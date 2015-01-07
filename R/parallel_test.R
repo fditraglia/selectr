a46e2e0976d41535a989abee93dca73454e7288b
Francis DiTraglia <fditra@sas.upenn.edu>
2015-01-06 22:41:44 -0500

    move sample and rep sizes inside of functions'

--- R/parallel_test.R
+++ R/parallel_test.R
@@ -3,27 +3,32 @@
 # in which the loads are perfectly balanced
 # and there shouldn't be any memory issues
 
+require(Rcpp)
+require(RcppArmadillo)
 require(parallel)
 max.cores <- detectCores()
 
-
-reps <- 1000
-n <- 1000
-x <- rnorm(n)
+sourceCpp("reg_MSE.cpp")
 
 reg_slope <- function(b){
+  n <- 1000
   x <- rnorm(n)
   y <- b * x + rnorm(n)
   return(cov(x, y) / var(x))
 }
 
 reg_MSE <- function(b){
+  reps <- 10000
   sims <- replicate(reps, reg_slope(b))
   return(mean((sims - b)^2))
 }
 
-run_sim <- function(cores, tasks){
+simR <- function(cores, tasks){
   b_seq <- seq(0.9, 1.1, length.out = tasks)  
   return(mclapply(b_seq, reg_MSE, mc.cores = cores))
 }
 
+simCpp <- function(cores, tasks){
+  b_seq <- seq(0.9, 1.1, length.out = tasks)  
+  return(mclapply(b_seq, reg_MSE_cpp, mc.cores = cores))
+}
