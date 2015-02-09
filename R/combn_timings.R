b095695224bf54303bbcce8274754fc9e8914c15
Francis DiTraglia <fditra@sas.upenn.edu>
2015-02-09 17:35:25 -0500

    start script to compare timings

---
+++ R/combn_timings.R
@@ -0,0 +1,16 @@
+# Compare performance of combn, mycombn, a byte-code
+# compiled verion of mycombn and an Rcpp version 
+library(microbenchmark, compiler)
+source("combn_stripped.R")
+
+# create byte-code compiled version of mycombn
+mycombnc <- cmpfun(mycombn)
+n <- 50 
+k <- 3
+system.time(foo <- combn(n, k))
+system.time(bar <- mycombn(n, k))
+system.time(baz <- mycombnc(n, k))
+all.equal(foo, bar)
+all.equal(foo, baz)
+
+microbenchmark(combn(n, k), mycombn(n, k), mycombnc(n, k))
